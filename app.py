1aa5f48334cc87e58dc232238e90074d19160f72
shreyagakhar <168405033+shreyagakhar@users.noreply.github.com>
2026-02-17 00:08:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import numpy as np
+
+df= pd.read_csv(r'C:\Users\Lenovo\Desktop\Mall_data\Deployment\Mall_df.csv')
+df= df.set_index('date')
+df.index = pd.to_datetime(df.index).normalize()
+df.columns= ['Past Sales']
+
+tes= pickle.load(open(r'C:\Users\Lenovo\Desktop\Mall_data\Deployment\tes.pkl', 'rb'))
+arima= pickle.load(open(r'C:\Users\Lenovo\Desktop\Mall_data\Deployment\arima.pkl', 'rb'))
+sarima= pickle.load(open(r'C:\Users\Lenovo\Desktop\Mall_data\Deployment\sarima.pkl', 'rb'))
+
+st.title('Predicting Overall Mall Sales')
+
+st.write('Select the number of days you need to forecast sales for:')
+days= st.slider('Number of days',1,30,1)
+
+selection= st.selectbox('Select the Model:', ['Holt-Winters', 'ARIMA', 'SARIMA'], None)
+
+if selection=='Holt-Winters':
+    model= tes
+elif selection=='ARIMA':
+    model= arima
+elif selection=='SARIMA':
+    model= sarima
+else:
+    model= None
+
+
+if st.button("Generate Forecast"):
+    if model== None:
+        st.error('Pleae select a model to generate a prediction')
+    else:
+        forecast = model.forecast(days)
+        forecast_df = pd.DataFrame({"Forecast": forecast})
+        new_df = pd.concat([df, forecast_df], axis=1)
+        st.line_chart(new_df, x_label='Dates', y_label='Sales', color=['blue', 'orange'])
+        st.dataframe(forecast_df)
+
+
